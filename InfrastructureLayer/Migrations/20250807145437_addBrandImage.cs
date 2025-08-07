d1eb9e6c3ad44fdca555cb4e5f9e0294e554693c
Shalva Gagnidze <salvagagnidze@gmail.com>
2025-08-07 18:54:59 +0400

    addBrandImage Migration

---
+++ InfrastructureLayer/Migrations/20250807145437_addBrandImage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfrastructureLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addBrandImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<List<string>>(
+                name: "Images",
+                table: "Brands",
+                type: "text[]",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Images",
+                table: "Brands");
+        }
+    }
+}
